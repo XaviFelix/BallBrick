b00c534d02aec14d1e7425330426aa2a125c04cb
XaviFelix <xavierfelix74@gmail.com>
2024-12-25 22:56:53 -0800

    created a brick matrix

--- utils/brick_list.lua
+++ utils/brick_list.lua
@@ -1,3 +1,5 @@
+local Brick = require("utils.brick")
+
 Brick_List = {}
 Brick_List.__index = Brick_List
 
@@ -17,6 +19,27 @@ end
 
 function Brick_List:insert(brick)
 	table.insert(self.list, brick)
+end
+
+-- NOTE: This currently works
+-- TODO: This needs more testing to ensure edge cases don't break it
+function Brick_List:constructLevel()
+	self.rows = 8
+	self.columns = 11
+	self.current_level_bricks = {}
+
+	local brick = Brick:new()
+
+	for row = 1, self.rows do
+		for col = 1, self.columns do
+			local new_brick_pos_x = brick.top_left_pos_x + (col - 1) * (brick.width + brick.horizontal_distance)
+			local new_brick_pos_y = brick.top_left_pos_y + (row - 1) * (brick.height + brick.vertical_distance)
+
+			local new_brick = Brick:new(new_brick_pos_x, new_brick_pos_y)
+
+			self:insert(new_brick)
+		end
+	end
 end
 
 function Brick_List:drawBricks()
