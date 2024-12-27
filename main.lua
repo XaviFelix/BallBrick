0272b779ad01faa9fde8b4f9d795afaf3a2686a3
XaviFelix <xavierfelix74@gmail.com>
2024-12-26 22:09:43 -0800

    modified scope of objects

--- main.lua
+++ main.lua
@@ -1,35 +1,38 @@
+local love = require("love")
 local Ball = require("utils.ball")
 local Platform = require("utils.platform")
 local Brick = require("utils.brick")
 local Brick_List = require("utils.brick_list")
 local input = require("utils.input")
 
+local platform = nil
+local brickList = nil
+local ball = nil
+
 -- Start of love framework
 
 function love.load()
-	-- Creating instances of the objects
 	brickList = Brick_List:new()
 	ball = Ball:new(300, 300, 10, 300, 300)
 	platform = Platform:new(500, 500, 70, 20, 300)
-	-- brick = Brick:new(100, 100, 50, 30)
-	-- brickList:insert(Brick:new(100, 100, 50, 30))
-	-- brickList:insert(Brick:new(160, 100, 50, 30))
 	brickList:constructLevel()
+
+	-- Debug code
+	brickList:printList()
 end
 
 function love.update(dt)
-	-- Move the ball
-	ball:update(dt)
-	brickList:updateBrick(dt)
-	-- Platform movement
-	input.movePlatform(dt)
+	if ball ~= nil and brickList ~= nil then
+		ball:update(dt)
+		brickList:updateBrick(dt)
+	end
+	input.movePlatform(platform, dt)
 end
 
 function love.draw()
-	ball:draw()
-	platform:draw()
-	brickList:drawBricks()
+	if ball ~= nil and platform ~= nil and brickList ~= nil then
+		ball:draw()
+		platform:draw()
+		brickList:drawBricks()
+	end
 end
-
--- NOTE:  Defined functions
--- TODO: Create a function that will populate a set of bricks for a arbritrary level
